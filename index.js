f412700c7cc3d7a6d671b76789d82530edf5e430
Chirag Agrawal <56640339+chiragagrawal026@users.noreply.github.com>
2022-10-06 04:11:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { post } from 'axios';
 
 export function squareAndSend (x) {
   // send x to example.com
-  post('http://google.com', x);
+//   post('http://google.com', x);
 
   return x * x;
 }
